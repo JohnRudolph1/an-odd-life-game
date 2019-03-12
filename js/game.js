406fa1ff9edcdfc4407704e53fa8e0f87c59b91c
Alec J Hawley <alech35@yahoo.com>
2019-03-12 00:36:22 -0400

    js update

---
+++ js/game.js
@@ -0,0 +1,34 @@
+/* Template for progressing story board ******************************************
+
+
+$('').click(function ({
+    $('').slideUp(1000, function (){
+        $('').slideDown(1000, function (){
+            $('').show();
+        })
+    })
+})
+
+
+*/
+//---------------------------------------------------------------------------------------------------------------------------------------
+console.log ('Start program');
+//Start button
+
+$('#startButton').click(function () {
+    alert('The place is here, the time is now...');
+    $('#jumboText').slideUp(1000, function(){
+        $('#mb-s1').slideDown(1000, function(){
+            $('#mb-s1').show();
+        })
+    })
+})
+
+//Question 1:
+$('#continueButtonS1').click(function () {
+    $('#mb-s1').slideUp(1000, function(){
+        $('#mb-q1').slideDown(1000, function(){
+            $('#mb-q1').show();
+        })
+    })
+})
