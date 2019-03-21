85bd0b63f7da15594655ce822bed8b021f1f8351
Alec J Hawley <alech35@yahoo.com>
2019-03-21 14:56:47 -0400

    Added new branches to game

--- js/game.js
+++ js/game.js
@@ -2,12 +2,13 @@
 
 
 $('#').click(function (){
-    $('#').slideUp(1000, function (){
-        $('#').slideDown(1000, function (){
+    $('#').fadeOut(500, function (){
+        $('#').fadeIn(500, function (){
             $('#').show();
         })
     })
 })
+
 
 <!-- DISPLAY ON ALL DIVS SET TO :NONE, REFER TO js/oddlife.js FOR ANIMATIONS -->
       <!-- DIV WIKI: 
@@ -26,7 +27,7 @@ console.log ('Start program');
 //Start button
 
 $('#startButton').click(function () {
-    alert('The place is here, the time is now...');
+    //alert('The place is here, the time is now...');
     $('#jumboText').fadeOut(1000, function(){
         $('#mb-s1').fadeIn(1000, function(){
             $('#mb-s1').show();
@@ -42,6 +43,7 @@ $('#continueButtonS1').click(function () {
         })
     })
 })
+    //Branch 1
     //b1-q1
     $('#b1-button').click(function (){
         $('#mb-q1').fadeOut(500, function (){
@@ -146,3 +148,62 @@ $('#continueButtonS1').click(function () {
                             })
                         })
                     })
+    /************************************* Template for progressing story board ******************************************
+    $('#').click(function (){
+        $('#').fadeOut(500, function (){
+            $('#').fadeIn(500, function (){
+                $('#').show();
+            })
+        })
+    })
+    */
+    //Branch 2
+    //b2-q1
+    $('#b2-button').click(function (){
+        $('#mb-q1').fadeOut(500, function (){
+            $('#b2-q1').fadeIn(500, function (){
+                $('#b2-q1').show();
+            })
+        })
+    })
+        //b2-b1 transfer
+        $('#b2-q1-button1').click(function (){
+            $('#b2-q1').fadeOut(500, function (){
+                $('#b1-q1').fadeIn(500, function (){
+                    $('#b1-q1').show();
+                })
+            })
+        })
+            //b2-b2-s1
+            $('#b2-q1-button2').click(function (){
+                $('#b2-q1').fadeOut(500, function (){
+                    $('#b2-b2-s1').fadeIn(500, function (){
+                        $('#b2-b2-s1').show();
+                    })
+                })
+            })
+            //b2-b2-q1
+            $('#b2-b2-s1-continuebutton').click(function (){
+                $('#b2-b2-s1').fadeOut(500, function (){
+                    $('#b2-b2-q1').fadeIn(500, function (){
+                        $('#b2-b2-q1').show();
+                    })
+                })
+            })
+                //b2-b2-b1-s1
+                $('#b2-b2-q1-button1').click(function (){
+                    $('#b2-b2-q1').fadeOut(500, function (){
+                        $('#b2-b2-b1-s1').fadeIn(500, function (){
+                            $('#b2-b2-b1-s1').show();
+                        })
+                    })
+                })
+                    //b2-b2-b1-b1-s1
+                    $('#b2-b2-b1-s1-continue').click(function (){
+                        $('#b2-b2-b1-s1').fadeOut(500, function (){
+                            $('#b2-b2-b1-b1-q1').fadeIn(500, function (){
+                                $('#b2-b2-b1-b1-q1').show();
+                            })
+                        })
+                    })
+
