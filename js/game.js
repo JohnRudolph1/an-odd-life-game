4c171f4ba2693c137a1c04bb54511d00550b8133
Alec J Hawley <alech35@yahoo.com>
2019-03-21 12:51:27 -0400

    Made all pages much more responsive and efficient to mobile first

--- js/game.js
+++ js/game.js
@@ -27,8 +27,8 @@ console.log ('Start program');
 
 $('#startButton').click(function () {
     alert('The place is here, the time is now...');
-    $('#jumboText').slideUp(1000, function(){
-        $('#mb-s1').slideDown(1000, function(){
+    $('#jumboText').fadeOut(1000, function(){
+        $('#mb-s1').fadeIn(1000, function(){
             $('#mb-s1').show();
         })
     })
@@ -36,112 +36,112 @@ $('#startButton').click(function () {
 
 //mb-q1
 $('#continueButtonS1').click(function () {
-    $('#mb-s1').slideUp(1000, function(){
-        $('#mb-q1').slideDown(1000, function(){
+    $('#mb-s1').fadeOut(500, function(){
+        $('#mb-q1').fadeIn(500, function(){
             $('#mb-q1').show();
         })
     })
 })
     //b1-q1
     $('#b1-button').click(function (){
-        $('#mb-q1').slideUp(1000, function (){
-            $('#b1-q1').slideDown(1000, function (){
+        $('#mb-q1').fadeOut(500, function (){
+            $('#b1-q1').fadeIn(500, function (){
                 $('#b1-q1').show();
             })
         })
     })
        //b1-b1-s1
         $('#b1-q1-button1').click(function (){
-            $('#b1-q1').slideUp(1000, function (){
-                $('#b1-b1-s1').slideDown(1000, function (){
+            $('#b1-q1').fadeOut(500, function (){
+                $('#b1-b1-s1').fadeIn(500, function (){
                     $('#b1-b1-s1').show();
                 })
             })
         })
        //b1-b1-s2
         $('#b1-b1-continuebutton').click(function (){
-            $('#b1-b1-s1').slideUp(1000, function (){
-                $('#b1-b1-s2').slideDown(1000, function (){
+            $('#b1-b1-s1').fadeOut(500, function (){
+                $('#b1-b1-s2').fadeIn(500, function (){
                     $('#b1-b1-s2').show();
                 })
             })
         })
        //b1-b1-q1
         $('#b1-b1-continuebutton2').click(function (){
-            $('#b1-b1-s2').slideUp(1000, function (){
-                $('#b1-b1-q1').slideDown(1000, function (){
+            $('#b1-b1-s2').fadeOut(500, function (){
+                $('#b1-b1-q1').fadeIn(500, function (){
                     $('#b1-b1-q1').show();
                 })
             })
         })
             //b1-b1-b1-s1
             $('#b1-b1-q1-button1').click(function (){
-                $('#b1-b1-q1').slideUp(1000, function (){
-                    $('#b1-b1-b1-s1').slideDown(1000, function (){
+                $('#b1-b1-q1').fadeOut(500, function (){
+                    $('#b1-b1-b1-s1').fadeIn(500, function (){
                         $('#b1-b1-b1-s1').show();
                     })
                 })
             })
             //b1-b1-b2-s1
             $('#b1-b1-q1-button2').click(function (){
-            $('#b1-b1-q1').slideUp(1000, function (){
-                $('#b1-b1-b2-s1').slideDown(1000, function (){
+            $('#b1-b1-q1').fadeOut(500, function (){
+                $('#b1-b1-b2-s1').fadeIn(500, function (){
                     $('#b1-b1-b2-s1').show();
                 })
             })
         })
             //b1-b1-b2-s2
             $('#b1-b1-b2-s1-continuebutton').click(function (){
-                $('#b1-b1-b2-s1').slideUp(1000, function (){
-                    $('#b1-b1-b2-s2').slideDown(1000, function (){
+                $('#b1-b1-b2-s1').fadeOut(500, function (){
+                    $('#b1-b1-b2-s2').fadeIn(500, function (){
                         $('#b1-b1-b2-s2').show();
                     })
                 })
             })
             //b1-b1-b1-e1
             $('#restart').click(function (){
-                $('#b1-b1-b1-s1').slideUp(1000, function (){
-                    $('#mb-s1').slideDown(1000, function (){
+                $('#b1-b1-b1-s1').fadeOut(500, function (){
+                    $('#mb-s1').fadeIn(500, function (){
                         $('#mb-s1').show();
                     })
                 })
             })
             //b1-b1-b2-e1
             $('#restart2').click(function (){
-                $('#b1-b1-b2-s2').slideUp(1000, function (){
-                    $('#mb-s1').slideDown(1000, function (){
+                $('#b1-b1-b2-s2').fadeOut(500, function (){
+                    $('#mb-s1').fadeIn(500, function (){
                         $('#mb-s1').show();
                     })
                 })
             })
                 //b1-b2-s1
                 $('#b1-q1-button2').click(function (){
-                    $('#b1-q1').slideUp(1000, function (){
-                        $('#b1-b2-s1').slideDown(1000, function (){
+                    $('#b1-q1').fadeOut(500, function (){
+                        $('#b1-b2-s1').fadeIn(500, function (){
                             $('#b1-b2-s1').show();
                         })
                     })
                 })
                 //b1-b2-e1
                 $('#restart3').click(function (){
-                    $('#b1-b2-s1').slideUp(1000, function (){
-                        $('#mb-s1').slideDown(1000, function (){
+                    $('#b1-b2-s1').fadeOut(500, function (){
+                        $('#mb-s1').fadeIn(500, function (){
                             $('#mb-s1').show();
                         })
                     })
                 })
                     //b1-b3-s1
                     $('#b1-q1-button3').click(function (){
-                        $('#b1-q1').slideUp(1000, function (){
-                            $('#b1-b3-s1').slideDown(1000, function (){
+                        $('#b1-q1').fadeOut(500, function (){
+                            $('#b1-b3-s1').fadeIn(500, function (){
                                 $('#b1-b3-s1').show();
                             })
                         })
                     })
                     //b1-b3-transfer
                     $('#b1-b3-s1-transfer').click(function (){
-                        $('#b1-b3-s1').slideUp(1000, function (){
-                            $('#b1-b1-s2').slideDown(1000, function (){
+                        $('#b1-b3-s1').fadeOut(500, function (){
+                            $('#b1-b1-s2').fadeIn(500, function (){
                                 $('#b1-b3-s2').show();
                             })
                         })
